b1bfb5576749ee7147980bfae44ec1dbb8f43e1f
Michael Naraiswar <naraiswar@gmail.com>
2019-03-07 05:21:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,12 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts key.to_s.capitalize + ":"
     value.each do |nkey, nvalue|
-      puts '  ' + nkey.to_s.capitalize.sub("_", " ") + ': ' + nvalue.join(", ")
+      #puts '  ' + nkey.to_s.capitalize.sub("_", " ") + ': ' + nvalue.join(", ")
+      holiday = nkey.sub("_", " ").split
+      holiday.each do |word|
+        word.capitalize!
+      end
+      puts '  ' + holiday.join(", ") + ': ' + nvalue.join(", ")
     end
   end
 end
